a27fe4641166645ea9af49c6af902c90fa6e4d97
ArpanVala <valaarpan05@gmail.com>
2025-06-19 22:32:29 +0530

    CORS solve1

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const  path = require('path');
 const express = require('express');
 const cors = require('cors');
 const {errorHandler} = require('./middleware/errorMiddleware');
@@ -8,13 +9,30 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = ['https://goalsetter-v1-1.onrender.com'];
+app.use(cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        console.log("❌ Blocked by CORS:", origin);
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    optionsSuccessStatus: 200
+  }));
+
+
 app.use(express.json());
-
+app.use(express.urlencoded({extended:true}));
+ 
 app.use('/api/user', require('./routes/userRoutes'));
-
 app.use('/api/goals',require('./routes/goalRoutes'));
-
 app.use('/api/categories', require('./routes/categoryRoutes'));
 
 app.use(errorHandler);
