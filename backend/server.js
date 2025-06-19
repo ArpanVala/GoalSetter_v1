5f7ed024010e19fca5b0e3344eb3ed8836f8142b
ArpanVala <valaarpan05@gmail.com>
2025-06-19 23:26:16 +0530

    CORS solve3

--- backend/server.js
+++ backend/server.js
@@ -37,15 +37,6 @@ app.use('/api/categories', require('./routes/categoryRoutes'));
 
 app.use(errorHandler);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client', 'build')));
-  
-    app.get('*', (req, res) =>
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    );
-  }
-  
-
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 }
